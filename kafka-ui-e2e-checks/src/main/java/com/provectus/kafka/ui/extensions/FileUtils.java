4b37f8d0bab551e4167eead30ce41064aee772e7
anezboretskiy <anezboretskiy@modeln.com>
2022-08-18 18:26:47 +0300

    Update FileUtils #2431

--- kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/extensions/FileUtils.java
+++ kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/extensions/FileUtils.java
@@ -1,7 +1,5 @@
 package com.provectus.kafka.ui.extensions;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.Nullable;
 import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -9,26 +7,21 @@ import java.nio.charset.StandardCharsets;
 
 import static org.apache.kafka.common.utils.Utils.readFileAsString;
 
-@Slf4j
 public class FileUtils {
 
-    @Nullable
     public static String getResourceAsString(String resourceFileName) {
         try {
             return IOUtils.resourceToString("/" + resourceFileName, StandardCharsets.UTF_8);
         } catch (IOException e) {
-            log.error("getResourceAsString() returns null", e);
-            return null;
+            throw new RuntimeException(e);
         }
     }
 
-    @Nullable
     public static String fileToString(String path) {
         try {
             return readFileAsString(path);
         } catch (IOException e) {
-            log.error("fileToString() returns null", e);
-            return null;
+            throw new RuntimeException(e);
         }
     }
 }
